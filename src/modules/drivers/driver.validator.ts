16314d07b0174ffa8b3eece8ded0ae987e5f4c90
PriyaZ02 <priyadharshini@zithmi.com>
2026-09-10 14:04:34 +0530

    Validation issue fix

--- src/modules/drivers/driver.validator.ts
+++ src/modules/drivers/driver.validator.ts
@@ -78,7 +78,11 @@ export const findNearbyDriversValidator = celebrate({
       .required()
       .messages({ 'any.required': 'Latitude is required for location search' }),
 
-    newTrip: TripValidation.createTripValidation.unknown(true).required(),
+    newTrip: TripValidation.createTripValidation.unknown(true).required()
+    .messages({
+      'any.required': 'New trip is required for location search',
+      'object.base': 'New trip must be an object',
+    }),
     // Radius: optional, defaults to 5km (5000 meters)
     radius: Joi.number()
       .min(100)
